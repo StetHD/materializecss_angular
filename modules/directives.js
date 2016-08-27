720a14a2f0f62261aaa4a81e93456b7d1cc5ff7a
Stet Howland <Stet Howland>
2016-08-27 10:19:45 +0430

    first commit

---
+++ modules/directives.js
@@ -0,0 +1,13 @@
+var app = angular.module("store-directives", []);
+
+app.directive("cardView", function () {
+
+    return {
+        restrict: "E",
+        templateUrl: "views/card-view.html",
+        controller: function () {
+
+
+        }
+    };
+});
